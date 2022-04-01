af79d3f9e4b6f60175cfe905c9d544fe592f59c7
Vilius Gudziunas <vilius.gudziunas@gmail.com>
2022-04-01 09:57:22 +0300

    Create config service for the app and typeorm

--- .eslintrc.js
+++ .eslintrc.js
@@ -14,5 +14,6 @@ module.exports = {
     '@typescript-eslint/explicit-module-boundary-types': 'off',
     '@typescript-eslint/interface-name-prefix': 'off',
     '@typescript-eslint/no-explicit-any': 'off',
+    'prettier/prettier': 'warn',
   },
 };
