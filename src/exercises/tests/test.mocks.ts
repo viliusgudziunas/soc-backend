cccce28de79257254216fe9238682c7b42c47e3e
Vilius Gudziunas <vilius.gudziunas@gmail.com>
2022-04-27 09:55:49 +0300

    Updated jest mocks with syntax sugar friendly functions

--- src/exercises/tests/test.mocks.ts
+++ src/exercises/tests/test.mocks.ts
@@ -4,47 +4,39 @@ import { ExercisesService } from '../exercises.service';
 import * as td from './test.data';
 
 export const mockExercisesService = {
-  findAll: jest.fn(() => Promise.resolve(td.mockExercises)),
-  findById: jest.fn(() => Promise.resolve(td.mockExercise)),
-  insert: jest.fn(() => Promise.resolve(td.mockExercise)),
-  update: jest.fn(() => Promise.resolve(td.mockUpdatedExercise)),
+  findAll: jest.fn().mockResolvedValue(td.mockExercises),
+  findById: jest.fn().mockResolvedValue(td.mockExercise),
+  insert: jest.fn().mockResolvedValue(td.mockExercise),
+  update: jest.fn().mockResolvedValue(td.mockUpdatedExercise),
 };
 
 export const mockRepository = {
-  find: jest.fn(() => Promise.resolve(td.mockExercises)),
-  findOne: jest.fn(() => Promise.resolve(td.mockExercise)),
-  insert: jest.fn(() => Promise.resolve(td.mockInsertExerciseResponse)),
-  update: jest.fn(() => Promise.resolve(td.mockUpdateExerciseResponse)),
+  find: jest.fn().mockResolvedValue(td.mockExercises),
+  findOne: jest.fn().mockResolvedValue(td.mockExercise),
+  insert: jest.fn().mockResolvedValue(td.mockInsertExerciseResponse),
+  update: jest.fn().mockResolvedValue(td.mockUpdateExerciseResponse),
 };
 
 export const mockFindOne = (
   repository: Repository<Exercise>,
   exercise: Exercise,
 ): jest.SpyInstance =>
-  jest
-    .spyOn(repository, 'findOne')
-    .mockImplementation(() => Promise.resolve(exercise));
+  jest.spyOn(repository, 'findOne').mockResolvedValue(exercise);
 
 export const mockInsertError = (
   repository: Repository<Exercise>,
   error: Error,
 ): jest.SpyInstance =>
-  jest.spyOn(repository, 'insert').mockImplementation(() => {
-    throw error;
-  });
+  jest.spyOn(repository, 'insert').mockRejectedValue(error);
 
 export const mockUpdate = (
   repository: Repository<Exercise>,
   result: UpdateResult,
 ): jest.SpyInstance =>
-  jest
-    .spyOn(repository, 'update')
-    .mockImplementation(() => Promise.resolve(result));
+  jest.spyOn(repository, 'update').mockResolvedValue(result);
 
 export const mockServiceFindById = (
   service: ExercisesService,
   exercise: Exercise,
 ): jest.SpyInstance =>
-  jest
-    .spyOn(service, 'findById')
-    .mockImplementation(() => Promise.resolve(exercise));
+  jest.spyOn(service, 'findById').mockResolvedValue(exercise);
